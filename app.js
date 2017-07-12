5abce563468133e7e05be08748dea9d0133bee4f
245743741@qq.com <245743741@qq.com>
2017-07-13 02:06:16 +0800

    票据access_token成功

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict' // 严格模式
 var Koa = require('koa')
+var path = require('path')
 var wechat = require('./wechat/g')
-var wechat_file = path.join(_dirname, './config/wechat.txt')
+var util = require('./libs/util')
+var wechat_file = path.join(__dirname, './config/wechat.txt')
 
 var config = {
 	wechat: {
@@ -12,13 +14,22 @@ var config = {
 			return util.readFileAsync(wechat_file)
 		},
 		saveAccessToken: function (data) { // 存
-			data = JSON.stringify(data)
+			var data = JSON.stringify(data)
 			return util.writeFileAsync(wechat_file, data)
 		}
 	}
 }
 
 var app = new Koa()
+
+app.use(function *(next) {
+	console.log(this.url)
+	if (this.url.indexOf('/movie') > -1) {
+		this.body = '<h1>nihao</h1>'
+		return next
+	}
+	yield next
+}) 
 
 app.use(wechat(config.wechat))
 
--- wechat/g.js
+++ wechat/g.js
@@ -1,22 +1,21 @@
 'use strict' // 严格模式
 var sha1 = require('sha1') // 加密
 var Promise = require('bluebird')
-var require = Promise.promisify(require('request')) // 一个请求库，通过promisify化而已
-
+var request = Promise.promisify(require('request')) // 一个请求库，通过promisify化而已
 var prefix = 'https://api.weixin.qq.com/cgi-bin/'
 var api = {
-	accessToken: prefix + 'token/grant_type=client_credential'
+	accessToken: prefix + 'token?grant_type=client_credential'
 }
 
-function Wechat(optns) {
+function Wechat(opts) {
 	var that = this
-	this.appID = opts.appID
+	this.appID = opts.appId
 	this.appSecret = opts.appSecret
 	this.getAccessToken = opts.getAccessToken // 获取
-	this.savaAccessToken = opts.savaAccessToken // 存储
+	this.saveAccessToken = opts.saveAccessToken // 存储
 
 	this.getAccessToken()
-		.then(function(data)) {
+		.then(function(data) {
 			try {
 				data = JSON.parse(data)
 			}
@@ -32,12 +31,11 @@ function Wechat(optns) {
 			}
 		})
 		.then(function(data) {
-			that.access_token = data.access_token
-			that.expires_in = data.expires_in
+			that.access_token = 'data.access_token'
+			that.expires_in = 'data.expires_in'
 
-			that.savaAccessToken(data)
+			that.saveAccessToken(data)
 		})
-	// body...
 }
 
 Wechat.prototype.isValidAccessToken = function(data) {
@@ -58,15 +56,20 @@ Wechat.prototype.isValidAccessToken = function(data) {
 }
 
 Wechat.prototype.updateAccessToken = function () { // 更新票据
+	// console.log(this.appSecret)
 	var appID = this.appID
 	var appSecret = this.appSecret
 	var url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret
 
 	return new Promise(function(resolve, reject) {
 		request({url: url, json: true}).then(function(response) {
-			var data = response[1]
+			// console.log(response)
+			// var data = response[1]
+			var data = response.body
+			// console.log(response)
+			// console.log(response.body)
 			var now = (new Date().getTime())
-			var expires_in = now + (data.expires_in -20) * 1000
+			var expires_in = now + (data.expires_in - 20) * 1000
 
 			data.expires_in = expires_in
 
@@ -76,7 +79,7 @@ Wechat.prototype.updateAccessToken = function () { // 更新票据
 }
 
 module.exports = function (opts) { // 加密认证中间件
-	var wechat = new Wechat(opts)
+	var wechat = new Wechat(opts) // 实例化构造函数
 
 	return function *(next) {
 		console.log(this.query)
